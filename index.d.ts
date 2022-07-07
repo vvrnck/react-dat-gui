f7a1d5f1f5f79895243879cea58312801c47804b
vvrnck <rwndm5@gmail.com>
2022-07-07 17:42:40 -0300

    adding className and style props on DatFolder interface - fixing TS

--- index.d.ts
+++ index.d.ts
@@ -56,6 +56,8 @@ export interface DatFolderProps extends DatUnchangableFieldProps {
   title: string;
   closed: boolean;
   children: any;
+  className?: string;
+  style?: React.CSSProperties;
 }
 
 export class DatFolder extends React.Component<DatFolderProps, any> {}
